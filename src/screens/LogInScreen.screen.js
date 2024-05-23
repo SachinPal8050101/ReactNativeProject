75d49530eb2022c34bea349a87cd0519f4120cdc
Sachin Kumar <sachin.kumar1@daffodilsw.com>
2024-05-23 19:45:24 +0530

    login screen design

--- src/screens/LogInScreen.screen.js
+++ src/screens/LogInScreen.screen.js
@@ -6,13 +6,14 @@ const LogInScreen = () => {
   const logInContext = useContext(LogInContext);
   console.log('==>>', logInContext);
   return (
-    <View>
-      <Text style={{color: 'black'}}>LogInScreen.screen</Text>
+    <View style={styles.container}>
+      <Text style={styles.noteText}>Login to Access the Application</Text>
       <Pressable
+        style={styles.btnCon}
         onPress={() => {
           logInContext.userLoggedIn();
         }}>
-        <Text style={{color: 'black'}}>LogIN</Text>
+        <Text style={styles.btnText}>LOGIN</Text>
       </Pressable>
     </View>
   );
@@ -20,4 +21,29 @@ const LogInScreen = () => {
 
 export default LogInScreen;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#F2F4F5',
+    justifyContent: 'center',
+  },
+  btnCon: {
+    backgroundColor: '#ED2D2F',
+    marginHorizontal: 30,
+    paddingVertical: 16,
+    borderRadius: 48,
+  },
+  btnText: {
+    color: '#FFFFFF',
+    alignSelf: 'center',
+    fontWeight: '700',
+    fontSize: 17,
+  },
+  noteText: {
+    alignSelf: 'center',
+    marginBottom: 20,
+    color: 'Black',
+    fontWeight: '900',
+    fontSize: 17,
+  },
+});
